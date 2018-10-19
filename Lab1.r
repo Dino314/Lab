8d02eca22fcf0f3909a5f98d86dcf8a4c58d8873
Dino314 <44084659+Dino314@users.noreply.github.com>
2018-10-19 12:53:12 +0200

    Update Lab1.r

--- Lab1.r
+++ Lab1.r
@@ -41,6 +41,88 @@ factor(x, levels=c(11, 22, 47), ordered=TRUE)
 z <- c("p", "a" , "g", "t", "b")
 z[3] <- "b"
 z
-#Exercize 4 = 
+#Exercize 4
 z <- factor(c("p", "q", "p", "r", "q"))
-z
+levels(z)
+levels(z)[levels(z)=="p"]<-"w"
+levels(z)
+#Exercise 5
+s1 <- factor(sample(letters, size=5, replace=TRUE))
+s2 <- factor(sample(letters, size=5, replace=TRUE))
+s3 <- factor(c(as.character(s1),as.character(s2)))
+#Exercise 6
+v1<-c(1:50)
+v1
+
+cut(v1, breaks = c(0, 10, 20, 30, 40, 50))
+
+v <- iris$Sepal.Length
+v
+
+f1<-cut(v, breaks=4.3+(0:5*0.72))
+f1
+m<-summary(f1)
+m
+#Exercise 8
+x <- data.frame(q=c(2, 4, 6), p=c("a", "b", "c"))
+levels(x$p) <-c("fertiliser1", "fertiliser2", "fertiliser3")
+x
+levels(x$p)
+#################################
+str(x)
+dim(x)
+class(x)
+ v<-iris$Sepal.Length
+v 
+v[v>6]
+
+mylist<-list(x=c(1,5,7), y=c(4,2,6), z=c(0,3,4))
+str(mylist)
+mylist$y[3]
+
+mylist[[2]][[3]]
+
+a<-matrix(1:9, nrow=3)
+a
+colnames(a) <- c("A", "B", "C")
+a
+a[1:2, 3]
+class(a[1:2, 3, drop=FALSE])
+
+cars
+
+cars[ ,2]
+
+colnames(airquality)
+head(airquality)
+
+head(airquality[, c(1, 3, 4)])
+cars[1:10, ]
+cars[cars$speed==4,]
+
+cars[cars$speed>10,2,drop=FALSE]
+
+cars[cars$speed>3 & cars$dist==2,]
+cars[cars$speed>3 | cars$dist==2,]
+
+df<-as.tibble(airquality[airquality$Temp>75 & 
+             airquality$Wind>12, c(1,5)])
+df
+na.omit(df)
+#########################
+#Exercise 1
+mtcars[mtcars[[1]]>15.0,1]
+#Exercise 2
+as.data.frame(mtcars[mtcars[[1]]>=21, ])
+#Exercise 3
+mtcars[mtcars$cyl<6 & mtcars$gear==4, ]
+#Exercise 4
+mtcars[mtcars$mpg>=21, 1:4]
+#Exercise 5
+head(airquality[airquality$Ozone>28 | airquality$Temp>70, ], 5)
+#Exercise 6
+head(airquality[airquality$Ozone>28 & airquality$Temp>70, c(1, 4)], 5)
+#Exercise 7
+head(CO2[CO2$Treatment=="chilled" & CO2$uptake>15, -4], 10)
+#Exercise 8
+airquality[airquality$Ozone>100, c("Ozone", "Temp", "Month", "Day")]
